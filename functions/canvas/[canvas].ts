73977fd0b28675f682ab589e7686dfca13d373b1
Jacob Hands <jacob@gogit.io>
2022-11-07 18:10:33 -0600

    init

---
+++ functions/canvas/[canvas].ts
@@ -0,0 +1,16 @@
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+  const url = new URL(request.url);
+  url.pathname = '/canvas'
+  const newReq = new Request(url.toString(), request);
+  return env.ASSETS.fetch(newReq)
+}
